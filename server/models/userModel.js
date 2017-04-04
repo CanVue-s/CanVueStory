98b2343abf3c9d8a496f062d058de771a17ee3c1
Kangseon Cho <kangseoncho@Kangseons-MacBook-Air.local>
2017-04-03 17:30:10 -0700

    set up Aelx's own DB

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -6,4 +6,5 @@ const userSchema = new Schema({
   notes: {type: String}
 });
 
+// collection is being created depending on what the export model is
 module.exports = mongoose.model('User', userSchema);
