f28bdbe8c977bc9150e0764b604a55c78fdd8bd7
Kevin Qiu <kami.kevinq@gmail.com>
2017-04-05 10:33:34 -0700

    finished dynmica creation of room as well as sockets in rooms only communicate with each other

--- client/home/home.js
+++ client/home/home.js
@@ -1,60 +1,21 @@
-// const URL = 'http://ec2-52-89-83-246.us-west-2.compute.amazonaws.com:3000';
-const URL = 'http://localhost:3000';
-let globalUserNum = 0;
-
-function sendObj(user, password, notes, canvas) {
-  var obj = {
-    user: user,
-    password: password,
-    notes: notes
-  }
-  return JSON.stringify(obj);
-}
-
-//used to make new user number
-function createUser(userNumber) {
-  $.ajax({
-    url: URL + '/create',
-    type: "POST",
-    data: sendObj('user' + userNumber.toString(), ''),
-    dataType: "json",
-    contentType: "application/json"
-  });
-}
-
-function getUserNumber() {
-  $.get(URL + '/notes/tracker', function (data) {
-    globalUserNum = parseInt(data.notes);
-  });
-}
-
 $(document).ready(function () {
 
-  // Check database at intervals for updates in user number
-  setInterval(function () {
-    getUserNumber();
-    if (globalUserNum === 1) $('#room1').text('Room 1:  ' + globalUserNum.toString() + ' User');
-    if (globalUserNum > 1) $('#room1').text('Room 1:  ' + globalUserNum.toString() + ' Users');
-  }, 500);
+  // setInterval(() => {
+  //   $('#room1').text('Room 1:  ' + localStorage.room1.toString() + ' Users');
+  //   $('#room1').text('Room 1:  ' + localStorage.room2.toString() + ' Users');
+  //   $('#room1').text('Room 1:  ' + localStorage.room3.toString() + ' Users');
+  // }, 500)
 
-  $('#rooms').on('click', 'a', function (event) {
-    let user = event.target.innerHTML.slice(9, 10);
-    user = parseInt(user) + 1;
+  // dynamically creates a new room
+  $('#create-room').submit((event) => {
+    event.preventDefault();
+    let roomName = $('#create-room input').val();
+    $('#rooms').append(`<div class='link-div well'><a class="eachRoom">${roomName}</a></div>`)
+    $('#create-room input').val('');
+  })
 
-    // create or update tracker to keep track of numbers of users.
-    $.ajax({
-      url: URL + '/notes/tracker',
-      type: "PUT",
-      data: sendObj('tracker', user), 
-      dataType: "json",
-      contentType: "application/json"
-    });
-
-    if (user === 1) $('#room1').text('Room 1:  ' + user.toString() + ' User');
-    if (user > 1) $('#room1').text('Room 1:  ' + user.toString() + ' Users');
-    let roomUrl = URL + "/rooms/room1" + 'user' + user.toString();
-    createUser(user);
-    window.open(roomUrl);
+  $('body').on('click', 'a.eachRoom', function() {
+    localStorage.room = $(this).text();
+    location.href = `http://localhost:3000/rooms/${$(this).text()}`
   });
-
 });
--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -1,30 +1,22 @@
 module.exports = (io) => {
 
-  let counter = 0;
-  let roomA = 'roomA';
-  let roomB = 'roomB';
   // On initial server connection, socket passed to onConnection function.
   io.on('connection', onConnection);
 
   function onConnection(socket) {
     console.log('socket connected', socket.id);
 
-    // this checks to see how many users are in roomA and moves you to room B if there are more than 3
-    if (counter >= 3) {
-      socket.join(roomB);
-      // this emits to all users that you just connected to roomB
-      socket.room = roomB;
-      io.sockets.in(roomB).emit('connectToRoom', `User joined ${roomA}`);
-      counter += 1;
-      console.log(counter);
-    } else {
-      socket.join(roomA);
-      // this emits to all users that you just connected to roomA
-      socket.room = roomA;
-      io.sockets.in(roomA).emit('connectToRoom', `User joined ${roomB}`);
-      counter += 1;
-      console.log(counter);
-    }
+    // var clients = {
+    //   room1: 0,
+    //   room2: 0,
+    //   room3: 0
+    // }
+
+    socket.on('join', (room) => {
+      socket.join(room);
+      socket.room = room;
+      console.log('joined room', room);
+    })
 
     // Listens to disconnect event
     socket.on('disconnect', () => {
@@ -34,9 +26,11 @@ module.exports = (io) => {
     //Waits for drawing emit from main.js THEN broadcasts & emits the data to socket in main.js (line 32)
     socket.on('drawing', (data) => {
       io.sockets.in(socket.room).emit('drawing', data);
-      // io.in(roomA).emit('drawing', data);
-      //socket.broadcast.emit('drawing', data)
     });
+
+    socket.on('chatMessage', (message) => {
+      io.sockets.in(socket.room).emit('chatMessage', (message));
+    })
 
     //Waits for cleared emit from canvas.html THEN broadcasts & emits data to socket in canvas.html (line 35)
     socket.on('cleared', (data) => socket.broadcast.emit('clearCanvas', data));
